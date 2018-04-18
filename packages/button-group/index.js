a20b0b725ab45ec50d8a4025abdcb9b14ea6a43f
夏添 <xiatian@suiyi.com.cn>
2018-04-18 15:04:11 +0800

    delete

--- packages/button-group/index.js
+++
@@ -1,6 +0,0 @@
-import VButtonGroup from '../button/src/button-group'
-
-VButtonGroup.install = function (Vue) {
-  Vue.component(VButtonGroup.name, VButtonGroup)
-}
-export default VButtonGroup
--- packages/container/index.js
+++
@@ -1,7 +0,0 @@
-import Container from './src/main'
-
-Container.install = function (Vue) {
-  Vue.component(Container.name, Container)
-}
-
-export default Container
